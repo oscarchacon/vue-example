bc294ff2450965947db91cf987f814f5160fb356
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-05-07 15:58:28 -0400

    Cambios Menores

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 
 Vue.config.productionTip = false
