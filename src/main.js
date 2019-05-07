e65c14cc74f24c4eb8307afbef0e25dfce5f7552
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-05-07 18:32:06 -0400

    uso de vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import './plugins/axios'
 import App from './App.vue'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
