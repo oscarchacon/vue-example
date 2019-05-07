4f57886a821e29453e24f47f199d57c0a1ba97a7
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-05-07 18:51:20 -0400

    Fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import './plugins/axios'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'font-awesome/css/font-awesome.css'
+import Vue from 'vue';
+import './plugins/vuetify';
+import './plugins/axios';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
